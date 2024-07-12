52bed379e5cf4d52cb828362c588c7163f976e41
PeskyPotato <peskypotato@protonmail.com>
2024-07-12 18:48:14 +0200

    Added category support
    - Category template for items and channel
    - Test cases for categories

--- rss_py/tests/test_features.py
+++ rss_py/tests/test_features.py
@@ -22,3 +22,113 @@ class TestDate(TestCase):
     </channel>
 </rss>"""
         )
+
+    def test_category(self):
+        r = rss_py.build(
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            categories=[
+                {
+                    "text": "transport/rail",
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Bob's blog</title>
+        <link>https://example.com/</link>
+        <description>A collection of Bob's thoughts.</description>
+        <category>transport/rail</category>
+    </channel>
+</rss>"""
+        )
+
+    def test_categories(self):
+        r = rss_py.build(
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            categories=[
+                {
+                    "text": "transport/rail",
+                },
+                {
+                    "text": "post",
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Bob's blog</title>
+        <link>https://example.com/</link>
+        <description>A collection of Bob's thoughts.</description>
+        <category>transport/rail</category>
+        <category>post</category>
+    </channel>
+</rss>"""
+        )
+
+    def test_categories_domain(self):
+        r = rss_py.build(
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            categories=[
+                {
+                    "text": "transport/rail",
+                    "domain": "tags"
+                },
+                {
+                    "text": "post",
+                    "domain": "template"
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Bob's blog</title>
+        <link>https://example.com/</link>
+        <description>A collection of Bob's thoughts.</description>
+        <category domain="tags">transport/rail</category>
+        <category domain="template">post</category>
+    </channel>
+</rss>"""
+        )
+
+    def test_category_item(self):
+        r = rss_py.build(
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            items=[
+                {
+                    'title': 'Post1',
+                    "categories":[
+                        {
+                            "text": "transport/rail",
+                        }
+                    ]
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Bob's blog</title>
+        <link>https://example.com/</link>
+        <description>A collection of Bob's thoughts.</description>
+        <item>
+            <title>Post1</title>
+            <category>transport/rail</category>
+        </item>
+    </channel>
+</rss>"""
+        )
