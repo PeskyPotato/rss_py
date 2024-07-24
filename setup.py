48fcc192a435aefe51d51c8be165ed36bfa4db9a
PeskyPotato <peskypotato@protonmail.com>
2024-07-24 20:05:32 +0200

    Indent setup.py and add details

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='rss_py',
-      version='0.1',
-      description='Write an RSS feed',
-      url='https://github.com/peskypotato/rss_py',
-      author='PeskyPotato',
-      license='MIT',
-      packages=['rss_py'],
-      install_packages=[
-          'Jinja2'
-      ],
-      zip_safe=False,
-      include_package_data=True)
+    version='0.1',
+    description='Write an RSS feed',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+    url='https://github.com/peskypotato/rss_py',
+    author='PeskyPotato',
+    license='MIT',
+    packages=['rss_py'],
+    install_packages=[
+        'Jinja2'
+    ],
+    zip_safe=False,
+    include_package_data=True)
