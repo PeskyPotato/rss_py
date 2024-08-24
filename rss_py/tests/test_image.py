ee8c701f2770352e3550945283293a6cec774d8f
PeskyPotato <peskypotato@protonmail.com>
2024-08-24 10:09:49 +0200

    Improved image validation and added tests
    - Check if integer is passsed for image dimensions
    - Added test for exception raising on non-integer values
    - Added test for image and item post
    - Moved image validation logic to new function

--- rss_py/tests/test_image.py
+++ rss_py/tests/test_image.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-
+from datetime import datetime, timezone
 import rss_py
 
 
@@ -115,6 +115,54 @@ class TestDate(TestCase):
             <height>31</height>
         </image>
         <atom:link href="http://example.com/rss.xml" rel="self" type="application/rss+xml" />
+    </channel>
+</rss>"""
+        )
+
+    def test_image_height_str(self):
+        self.assertRaises(
+            Exception,
+            rss_py.build,
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            image = {
+                "url": "http://example.com/static/header.png",
+                "height": "15"
+            }
+        )
+
+    def test_image_post(self):
+        r = rss_py.build(
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            image = {
+                "url": "http://example.com/static/header.png",
+            },
+            items=[
+                {
+                    "title": "Post 1",
+                    "pubDate": datetime(2024, 6, 21, 7, 18, 32, tzinfo=timezone.utc)
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Bob's blog</title>
+        <link>https://example.com/</link>
+        <description>A collection of Bob's thoughts.</description>
+        <image>
+            <url>http://example.com/static/header.png</url>
+            <title>Bob's blog</title>
+            <link>https://example.com/</link>
+        </image>
+        <item>
+            <title>Post 1</title>
+            <pubDate>Fri, 21 Jun 2024 07:18:32 +0000</pubDate>
+        </item>
     </channel>
 </rss>"""
         )
