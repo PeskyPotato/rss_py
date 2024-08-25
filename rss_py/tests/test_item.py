1863df0b2abac78c692984485d62aaf347a20141
PeskyPotato <35942108+PeskyPotato@users.noreply.github.com>
2024-08-25 16:59:54 +0200

    Added source sub-element in item (#12)
    - Added source to item
    - Checks if URL parameter is present in source
    - Added tests for source
    
    Closes #10

--- rss_py/tests/test_item.py
+++ rss_py/tests/test_item.py
@@ -32,3 +32,84 @@ class TestItem(TestCase):
     </channel>
 </rss>"""
         )
+
+    def test_item_source(self):
+        r = rss_py.build(
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            items=[
+                {
+                    "title": "Post 1",
+                    "pubDate": datetime(2024, 6, 21, 7, 18, 32, tzinfo=timezone.utc),
+                    "source": {
+                        "url": "https://example.com/",
+                        "title": "Title for item"
+                    }
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Bob's blog</title>
+        <link>https://example.com/</link>
+        <description>A collection of Bob's thoughts.</description>
+        <item>
+            <title>Post 1</title>
+            <pubDate>Fri, 21 Jun 2024 07:18:32 +0000</pubDate>
+            <source url="https://example.com/">Title for item</source>
+        </item>
+    </channel>
+</rss>"""
+        )
+
+    def test_item_source_no_url(self):
+        self.assertRaises(
+            Exception,
+            rss_py.build,
+            title="Bob's blog",
+            link="https://example.com/",
+            description="A collection of Bob's thoughts.",
+            items=[{
+                "title": "Post with a source",
+                "pubDate": datetime(2024, 6, 21, 7, 18, 32, tzinfo=timezone.utc),
+                "source": {
+                    "title": "Title for item"
+                }
+            }]
+        )
+
+    def test_item_source_no_title(self):
+        r = rss_py.build(
+            title="Example feed",
+            link="https://example.com/",
+            description="A feed simply for example purposes.",
+            items=[
+                {
+                    "title": "The first post",
+                    "pubDate": datetime(2024, 8, 22, 6, 49, 12, tzinfo=timezone.utc),
+                    "description": "This posts source doesn't have text but that seems to be okay.",
+                    "source": {
+                        "url": "https://example.com/post.xml",
+                    }
+                }
+            ]
+        )
+        self.assertEqual(r,
+            """<?xml version="1.0"?>
+<rss version="2.0">
+    <channel>
+        <title>Example feed</title>
+        <link>https://example.com/</link>
+        <description>A feed simply for example purposes.</description>
+        <item>
+            <title>The first post</title>
+            <description>This posts source doesn&#39;t have text but that seems to be okay.</description>
+            <pubDate>Thu, 22 Aug 2024 06:49:12 +0000</pubDate>
+            <source url="https://example.com/post.xml"></source>
+        </item>
+    </channel>
+</rss>"""
+        )
